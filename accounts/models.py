81f69968c40910ef251f23bbcf8642a06731b5df
gmbt18 <84000377+gmbt18@users.noreply.github.com>
2022-08-14 19:05:31 +0800

    remove tktinker

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,4 @@
 from time import timezone
-from tkinter import CASCADE
 from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
