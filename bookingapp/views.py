ed811a8a74ae9605d3b7bddee255317f3fecfc98
gmbt18 <84000377+gmbt18@users.noreply.github.com>
2022-08-14 19:07:27 +0800

    modified views.py of bookingapp

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, JsonResponse
 from django.template.loader import render_to_string
