bf5cb40efb215511e2a890bb4dac191427c22ca3
gmbt18 <84000377+gmbt18@users.noreply.github.com>
2022-08-14 19:45:05 +0800

    updated settings

--- ORB/settings.py
+++ ORB/settings.py
@@ -141,9 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 django_heroku.settings(locals())
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
