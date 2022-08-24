e2f651b5142d0af473ac02b5d0c4c350cca6dbb5
gmbt18 <gabrieluntalan12@gmail.com>
2022-08-24 16:49:14 +0800

    added logo to login

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     
@@ -35,3 +37,5 @@ urlpatterns = [
     path('delete/<int:pk>',views.deleteUser.as_view(), name='deleteUser'),
    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
