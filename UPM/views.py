398572319fc4055111ee4a0a4ab10a05a04be83e
gmbt18 <gabrieluntalan12@gmail.com>
2022-08-24 18:39:37 +0800

    added an additional condition to upload the schedule

--- UPM/views.py
+++ UPM/views.py
@@ -98,7 +98,7 @@ def uploadPage(request):
                 arr=[]
                 arr[:0]=days
                 
-                if Schedule.objects.filter(Q(room=room), Q(time_start__range=(st_range,et_range))|Q(time_end__range=(st_range,et_range)),Q(dayofweek__contains=(arr[0] or arr[1]))).exists():
+                if Schedule.objects.filter(Q(schedfile=file), Q(room=room), Q(time_start__range=(st_range,et_range))|Q(time_end__range=(st_range,et_range)),Q(dayofweek__contains=(arr[0] or arr[1]))).exists():
                     error= '"' + ct[i] + ' ' + sec[i] + ' (' + com[i] + ')" that has conflicts with other schedules.'
                 else:
                     Schedule.objects.create(
