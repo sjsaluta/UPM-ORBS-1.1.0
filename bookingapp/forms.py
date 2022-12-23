16526354bb06761028aacb7c1eba1663b66ec124
sjsaluta <100128901+sjsaluta@users.noreply.github.com>
2022-12-23 21:25:55 +0800

    Equipment, Google login, Multidate picker update

--- bookingapp/forms.py
+++ bookingapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import DateInput
+from django.forms import TimeInput#New
 from django.utils.translation import gettext_lazy as _
 from django.forms import ModelChoiceField, ModelForm,EmailField
 from django.core.validators import EmailValidator
@@ -7,28 +8,41 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from .models import *
 from .widgets import *
+from UPM.models import *
 
 
 class AddBookFrCal(ModelForm):
+    equipment = forms.ModelMultipleChoiceField(
+        queryset = Equipment.objects.all(),
+        widget = forms.CheckboxSelectMultiple,
+        required=False
+    )
     class Meta:
         model = Booking
-        fields = ['faculty','subject','start_time','end_time','numofstudents','activity','equipment','dept_or_office','organization']
+        fields = ['faculty','subject','start_time','end_time','date_picked','numofstudents','activity','equipment','dept_or_office','organization']
 
         labels={
             'numofstudents':'Number of Students',
             'start_time':'Start Time',
             'end_time':'End Time',
+            'date_picked': 'Date Selected',
             'dept_or_office':'Dept/Office'
         }
         widgets={
-            "start_time": DateInput(
-                attrs={"type": "datetime-local", "class": "form-control"},
-                format="%Y-%m-%dT%H:%M",
+            "start_time": TimeInput(#DateTimeInput
+                attrs={"type": "time", "class": "form-control"},#"type": "datetime-local"
+                #format="%Y-%m-%dT%H:%M",
+                format="T%H:%M",
             ),
-            "end_time": DateInput(
-                attrs={"type": "datetime-local", "class": "form-control"},
-                format="%Y-%m-%dT%H:%M",
+            "end_time": TimeInput(#DateTimeInput
+                attrs={"type": "time", "class": "form-control"},#"type": "datetime-local"
+                #format="%Y-%m-%dT%H:%M",
+                format="T%H:%M",
             ), 
+            "date_picked": DateInput(#New widget
+                attrs={"type": "date", "class": "form-control"},
+                format="%Y-%m-%d"
+            )
         }
 
 class EditBookingForm(ModelForm):
