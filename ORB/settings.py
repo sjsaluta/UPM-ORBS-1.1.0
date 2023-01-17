a34637e5df1d279c58f516cc6a091e421f435df8
sjsaluta <100128901+sjsaluta@users.noreply.github.com>
2023-01-17 21:04:48 +0800

    Updated requirements

--- ORB/settings.py
+++ ORB/settings.py
@@ -189,7 +189,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate
-web: gunicorn 'ORBS.wsgi'
+web: python manage.py migrate && gunicorn 'ORBS.wsgi'
